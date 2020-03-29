d7a2af9d46ac9449263733ecd656a4dbce95c841
sharif <sharif@sharif-PC>
2020-03-30 03:59:53 +0530

    hello this is my first push

---
+++ PROG 1/src/Hacker/myHack.java
@@ -0,0 +1,12 @@
+package Hacker;
+
+import example1.MyFirstClass;
+
+public class myHack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello"); 
+	}
+
+}
